336dc4371c81806fb62b59f83bd6163f4c1cf184
bdftyousra@gmail.com <bdftyousra@gmail.com>
2024-03-08 03:28:46 -0800

    Travel Management System Project Overview

--- pythonfiles/p.py
+++
@@ -1,7 +0,0 @@
-#!C:\Users\Rachid\AppData\Local\Programs\Python\Python310\python.exe
-import socket
-import requests
-"""hostname=socket.gethostname()
-ipAddr=socket.gethostbyname(hostname)"""#had l partie katjib l ip adresse sa3a jbart bli aslan had lien kayjibo l raso!
-reponse=requests.get("http://ip-api.com/json/").json()
-print(reponse['country']+" "+reponse['city'])
